deb419249ec2af4ee2707773cf50b1b304c85500
salmancz <salman.jsdev@gmail.com>
2022-05-24 16:57:48 +0530

    initializing engine

---
+++ mark.py
@@ -0,0 +1,14 @@
+import pyttsx3
+
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty("voices")
+engine.setProperty("voices", voices[0].id)
+
+def speak(audio):
+    engine.say(audio)
+    print(audio)
+    engine.runAndWait()
+
+
+if __name__ == "__main__":
+    speak("Hello Sir")
